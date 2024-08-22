fc57a40973b4d235dfffd2fabbe3240bfffcdc5f
Onur <onurozet@hotmail.com>
2024-08-22 18:37:09 +0300

    nextauth routes fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,51 @@
+import userModel from "@/models/users/userModel";
+import CredentialsProvider from "next-auth/providers/credentials";
+import dbConnect from "@/database/mongo";
+import NextAuth, {SessionStrategy} from "next-auth";
 
-import { authOptions } from "@/utils/authOptions";
-import NextAuth from "next-auth/next";
+export const authOptions= {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: {},
+                password: {}
+            },
+            async authorize(credentials,req) {
+                if (!credentials) {
+                    throw new Error("Credentials are missing");
+                }
+                await dbConnect();
+                const email = credentials.email;
+                const password = credentials.password;
+                const user = await userModel.findOne({ email: email });
 
+                if(!user){
+                    return null;
+                }
+
+                const isPasswordValid = user.password === password;
+
+                if (!isPasswordValid) {
+                    return null;
+                }
+
+                if(user){
+                    return user;
+                }
+            }
+        })
+    ],
+    session: {
+        strategy: 'jwt' as SessionStrategy,
+    },
+    pages: {
+        signIn: '/login',
+        error: '/auth/error'
+    },
+    secret:process.env.NEXTAUTH_SECRET,
+}
 
 const handler = NextAuth(authOptions);
 
-export { handler as POST };
-
-
-
-
-
-
+export { handler as GET, handler as POST };
